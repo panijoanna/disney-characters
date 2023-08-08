b6ffa18d69f5cfbed14c4f51acd454002f42882a
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-08 22:02:20 +0200

    feat: add VNavigation component with styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
-<script setup></script>
+<script setup>
+import VNavigation from "./components/VNavigation.vue";
+</script>
 
-<template></template>
+<template>
+  <VNavigation />
+</template>
---
+++ src/components/VNavigation.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <nav class="bg-white flex gap-12 pl-10 py-5">
+    <h1 class="uppercase font-bold">Disney</h1>
+    <ul>
+      <li class="hover:text-gray-500 cursor-pointer">My favorites</li>
+    </ul>
+  </nav>
+</template>
+
+<style></style>
