c2d8755f15a7deb00dd47a110efe37608f62b5a4
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-09 11:28:11 +0200

    feat: create VPopularCharacters component

---
+++ src/components/VPopularCharacters.vue
@@ -0,0 +1,29 @@
+<script setup>
+import BaseContainer from "./BaseContainer.vue";
+</script>
+
+<template>
+  <section class="flex justify-center items-center flex-col p-8">
+    <h1 class="text-2xl font-bold p-7">Most popular Disney Characters</h1>
+    <div class="flex gap-8">
+      <BaseContainer>
+        <img />
+        <h1>Character name</h1>
+        <span>Films</span>
+        <span>Tv shows</span>
+      </BaseContainer>
+      <BaseContainer>
+        <img />
+        <h1>Character name</h1>
+        <span>Films</span>
+        <span>Tv shows</span>
+      </BaseContainer>
+      <BaseContainer>
+        <img />
+        <h1>Character name</h1>
+        <span>Films</span>
+        <span>Tv shows</span>
+      </BaseContainer>
+    </div>
+  </section>
+</template>
