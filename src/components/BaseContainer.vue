ac682506d1ac14434c8572ea02ac4a0cd3bab2c6
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-09 14:26:34 +0200

    fix: change styles in BaseContainer

--- src/components/BaseContainer.vue
+++ src/components/BaseContainer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-white shadow-md w-52 h-60 flex flex-col p-3">
+  <div
+    class="bg-white shadow-md w-52 h-60 flex flex-col p-3 hover:scale-105 transition-transform duration-500"
+  >
     <slot></slot>
   </div>
 </template>
