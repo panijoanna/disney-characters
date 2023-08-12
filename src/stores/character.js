8d4226d1f61ea55e32f8dda502023db560e36442
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-12 10:24:39 +0200

    feat: create toggleFavoriteCharacterById function

--- src/stores/character.js
+++ src/stores/character.js
@@ -4,21 +4,28 @@ import axios from "axios";
 
 export const useCharacterStore = defineStore("character", () => {
   const characters = ref([]);
+  const favoriteCharacterIds = ref(new Set());
+
+  const toggleFavoriteCharacterById = (id) => {
+    if (favoriteCharacterIds.value.has(id)) {
+      favoriteCharacterIds.value.delete(id);
+    } else {
+      favoriteCharacterIds.value.add(id);
+    }
+  };
 
   const loadCharacterData = async () => {
-    try {
-      const response = await axios.get(
-        "https://api.disneyapi.dev/character?pageSize=100"
-      );
-      characters.value = response.data.data;
-    } catch (error) {
-      error.value = error;
-    }
+    const response = await axios.get(
+      "https://api.disneyapi.dev/character?pageSize=100"
+    );
+    characters.value = response.data.data;
   };
 
   loadCharacterData();
 
   return {
     characters,
+    toggleFavoriteCharacterById,
+    favoriteCharacterIds,
   };
 });
