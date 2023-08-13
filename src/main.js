8c57659bf6a6818091b7ce52066f503fa107e3e1
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-13 13:31:31 +0200

    feat: create router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import "./style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
+import router from "./router";
 
-createApp(App).use(createPinia()).mount("#app");
+createApp(App).use(createPinia()).use(router).mount("#app");
