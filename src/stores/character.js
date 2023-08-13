caa0958e898c7e6376d933b8c5ecb68530548171
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-13 10:05:33 +0200

    feat: create sorPopularCharacters function

--- src/stores/character.js
+++ src/stores/character.js
@@ -5,6 +5,7 @@ import axios from "axios";
 export const useCharacterStore = defineStore("character", () => {
   const characters = ref([]);
   const favoriteCharacterIds = ref(new Set());
+  const mostPopularCharacters = ref([]);
 
   const toggleFavoriteCharacterById = (id) => {
     if (favoriteCharacterIds.value.has(id)) {
@@ -18,11 +19,19 @@ export const useCharacterStore = defineStore("character", () => {
     characters.value.filter(({ _id }) => favoriteCharacterIds.value.has(_id))
   );
 
+  const sortPopularCharacters = () => {
+    mostPopularCharacters.value = characters.value
+      .filter((character) => character.films.length)
+      .sort((a, b) => b.films.length - a.films.length)
+      .slice(0, 3);
+  };
+
   const loadCharacterData = async () => {
     const response = await axios.get(
       "https://api.disneyapi.dev/character?pageSize=100"
     );
     characters.value = response.data.data;
+    sortPopularCharacters();
   };
 
   loadCharacterData();
@@ -32,5 +41,6 @@ export const useCharacterStore = defineStore("character", () => {
     toggleFavoriteCharacterById,
     favoriteCharacterIds,
     favoriteCharacters,
+    mostPopularCharacters,
   };
 });
