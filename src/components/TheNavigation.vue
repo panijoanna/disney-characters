391905f8f41d9241c95198e9819a4394e5901da4
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-13 13:32:41 +0200

    feat: add RouterLink to the TheNavigation

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,11 +1,17 @@
-<script></script>
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
 
 <template>
-  <nav class="bg-black text-white flex gap-12 pl-10 py-5 items-center">
-    <h1 class="uppercase font-bold text-xl">Disney</h1>
-    <ul>
-      <li class="hover:text-gray-400 cursor-pointer text-sm">My favorites</li>
-    </ul>
+  <nav class="bg-black text-white flex gap-2 p-5 items-center">
+    <Icon icon="tabler:brand-disney" color="white" width="30" />
+    <h1 class="uppercase font-bold text-xl tracking-wide">Disney</h1>
+    <div class="gap-8 flex ml-20">
+      <RouterLink to="/" class="hover:text-gray-400">Home</RouterLink>
+      <RouterLink to="/favorites" target="_blank" class="hover:text-gray-400"
+        >My favorites</RouterLink
+      >
+    </div>
   </nav>
 </template>
 
