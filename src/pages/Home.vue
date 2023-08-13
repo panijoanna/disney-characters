fbe025baddb06221be5c76166148d2c8047a6f41
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-13 13:32:17 +0200

    feat: create pages

---
+++ src/pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheDisneyCharacters from "../components/TheDisneyCharacters.vue";
+import ThePopularCharacters from "../components/ThePopularCharacters.vue";
+import TheSearchCharacter from "../components/TheSearchCharacter.vue";
+</script>
+
+<template>
+  <ThePopularCharacters />
+  <TheDisneyCharacters />
+  <TheSearchCharacter />
+</template>
