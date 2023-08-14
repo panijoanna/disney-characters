85b09c833aff1bb3a82670a25e2082cccc4dc5aa
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-14 11:38:19 +0200

    feat: add responsiveness to TheSearchCharacter

--- src/components/TheSearchCharacter.vue
+++ src/components/TheSearchCharacter.vue
@@ -17,16 +17,21 @@ const filteredCharacters = computed(() =>
 
 <template>
   <section class="flex justify-center mt-26">
-    <div class="flex flex-col items-center p-5 gap-6">
-      <Icon icon="mingcute:user-search-line" color="gray" width="40" />
+    <div class="flex flex-col items-center p-5 gap-6 max-lg:p-0">
+      <Icon
+        icon="mingcute:user-search-line"
+        color="gray"
+        width="40"
+        class="max-sm:mt-8"
+      />
       <h2 class="text-gray-500 text-lg">Look through your favorites:</h2>
       <input
-        class="p-2 w-96 border border-gray-200 rounded mb-6"
+        class="p-2 w-96 border border-gray-200 rounded mb-6 max-md:w-72 max-sm:w-1/2"
         placeholder="Search character..."
         type="text"
         v-model="search"
       />
-      <h1 class="font-bold text-2xl">My favorites characters</h1>
+      <h1 class="font-bold text-2xl max-sm:text-lg">My favorites characters</h1>
       <BaseCharacterList :list="filteredCharacters" title="Favorites" />
     </div>
   </section>
