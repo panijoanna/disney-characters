6d66edd767ec7c2bed2cb25ad61086b913a2e63f
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-14 11:37:41 +0200

    feat: add responsiveness to TheDisneyCharacters

--- src/components/TheDisneyCharacters.vue
+++ src/components/TheDisneyCharacters.vue
@@ -6,8 +6,8 @@ const characterStore = useCharacterStore();
 </script>
 
 <template>
-  <section class="mt-40">
-    <h1 class="text-3xl font-bold p-5 flex justify-center">
+  <section class="mt-40 max-md:mt-12">
+    <h1 class="text-3xl font-bold p-5 flex justify-center max-md:text-lg">
       What are your favorite characters?
     </h1>
     <BaseCharacterList
