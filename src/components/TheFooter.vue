512d248293e15ca2ee9b49d6e31bcdb03d9f2962
Joanna Panimasz <pani.joanna93@gmail.com>
2023-08-14 11:37:50 +0200

    feat: add responsiveness to TheFooter

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,7 +4,7 @@ import { Icon } from "@iconify/vue";
 
 <template>
   <footer
-    class="flex justify-center items-center text-base gap-2 h-20 bg-black"
+    class="flex justify-center items-center text-base gap-2 h-20 bg-black max-lg:mt-10"
   >
     <span class="text-white">Made with passion 2023</span>
     <Icon icon="pixelarticons:heart" color="red" />
